197bd4fe475e372ec4c653bafbedd35a3694b2e9
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2016-12-01 10:03:11 +0300

    Kdv Hesaplama Programı
    Kdvleri Hesaplayabilirsiniz.

---
+++ KdvHesaplama/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace KdvHesaplama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nmr_kdv = new System.Windows.Forms.NumericUpDown();
+            this.txt_urnfyt = new MetroFramework.Controls.MetroTextBox();
+            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nmr_kdv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(150, 187);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(90, 33);
+            this.metroButton1.TabIndex = 3;
+            this.metroButton1.Text = "Hesapla";
+            this.metroButton1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(27, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ürün Fiyatı";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(363, 18);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(194, 164);
+            this.webBrowser1.TabIndex = 3;
+            this.webBrowser1.Url = new System.Uri("http://www.ebubekirbastama.com", System.UriKind.Absolute);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(75, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "KDV";
+            // 
+            // nmr_kdv
+            // 
+            this.nmr_kdv.Location = new System.Drawing.Point(132, 115);
+            this.nmr_kdv.Name = "nmr_kdv";
+            this.nmr_kdv.Size = new System.Drawing.Size(108, 20);
+            this.nmr_kdv.TabIndex = 1;
+            this.nmr_kdv.Value = new decimal(new int[] {
+            18,
+            0,
+            0,
+            0});
+            // 
+            // txt_urnfyt
+            // 
+            this.txt_urnfyt.Lines = new string[0];
+            this.txt_urnfyt.Location = new System.Drawing.Point(132, 85);
+            this.txt_urnfyt.MaxLength = 32767;
+            this.txt_urnfyt.Name = "txt_urnfyt";
+            this.txt_urnfyt.PasswordChar = '\0';
+            this.txt_urnfyt.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txt_urnfyt.SelectedText = "";
+            this.txt_urnfyt.Size = new System.Drawing.Size(108, 19);
+            this.txt_urnfyt.TabIndex = 0;
+            this.txt_urnfyt.UseSelectable = true;
+            // 
+            // metroTextBox1
+            // 
+            this.metroTextBox1.Lines = new string[0];
+            this.metroTextBox1.Location = new System.Drawing.Point(132, 144);
+            this.metroTextBox1.MaxLength = 32767;
+            this.metroTextBox1.Name = "metroTextBox1";
+            this.metroTextBox1.PasswordChar = '\0';
+            this.metroTextBox1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox1.SelectedText = "";
+            this.metroTextBox1.Size = new System.Drawing.Size(108, 19);
+            this.metroTextBox1.TabIndex = 2;
+            this.metroTextBox1.UseSelectable = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(30, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 18);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Kdvli Fiyat";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(243, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 23);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "₺";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(246, 112);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 23);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "%";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(38, 34);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(234, 25);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Yazılım Eğitim Kanalı";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 243);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.metroTextBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_urnfyt);
+            this.Controls.Add(this.nmr_kdv);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.metroButton1);
+            this.Name = "Form1";
+            this.Style = MetroFramework.MetroColorStyle.Red;
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nmr_kdv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown nmr_kdv;
+        private MetroFramework.Controls.MetroTextBox txt_urnfyt;
+        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ KdvHesaplama/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+
+namespace KdvHesaplama
+{
+    public partial class Form1 : MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<double> degerler = new List<double>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Hide();
+        }
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            degerler.Add(Convert.ToDouble(txt_urnfyt.Text)); degerler.Add(Convert.ToDouble(nmr_kdv.Value));
+            degerler.Add(Convert.ToDouble(degerler[0]+(degerler[0] * degerler[1] / 100)));
+            metroTextBox1.Text = degerler[2].ToString();
+            degerler.Clear();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://www.ebubekirbastama.com");
+        }
+    }
+}
